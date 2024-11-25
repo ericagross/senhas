f0e0acb42efc51cb3214edc9788f6e040b59f922
Érica Gross <128057913+ericagross@users.noreply.github.com>
2024-11-25 11:45:28 -0300

    Add files via upload

--- erica/main.js
+++ erica/main.js
@@ -1,5 +1,34 @@
 const numeroSenha= document.querySelector('.parametro-senha-texto');
 let tamanhoSenha=10;
 numeroSenha.textContent=tamanhoSenha;
-const botoes= document.querySelector('.parametro-senha-botoes');
+const botoes= document.querySelectorAll('.parametro-senha-botao');
+const campoSenha= document.querySelector('#campo-senha');
+const letrasMaiusculas= "ABCDEFGHIJKLMNOPQRSTUVWXYZÇ";
 botoes[0].onclick= diminuirTamanho;
+botoes[1].onclick= aumentarTamanho;
+geraSenha();
+
+function diminuirTamanho(){
+    if(tamanhoSenha>5){
+            tamanhoSenha= tamanhoSenha-1;
+    numeroSenha.textContent=tamanhoSenha;
+    }
+}
+
+function aumentarTamanho(){
+    if(tamanhoSenha<20){
+    tamanhoSenha= tamanhoSenha+1;
+    numeroSenha.textContent=tamanhoSenha;
+}
+}
+
+
+function geraSenha(){
+    let senha= " ";
+    for (let i=0; i<tamanhoSenha; i++){
+    let numeroAleatorio= Math.random()* letrasMaiusculas.length;
+    numeroAleatorio= Math.floor(numeroAleatorio);
+    senha= senha+ letrasMaiusculas[numeroAleatorio];
+    }
+    campoSenha.value=senha;
+}
